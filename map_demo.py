b179676266aee99e01a073acec3e89091ad176dc
Kresnananta <akresnaananta@gmail.com>
2025-12-14 21:42:24 +0700

    add feature: Export & Import to JSON, fixing connection bug

--- map_demo.py
+++ map_demo.py
@@ -1,16 +1,19 @@
 import tkinter as tk
 import os
+import json
 import math
 import heapq
-from tkinter import simpledialog, messagebox
+from tkinter import simpledialog, messagebox, filedialog
 from PIL import Image, ImageTk
 
 canvas_w = 800
 canvas_h = 600
 pin_node_rad = 10
-node_rad = 5
+node_rad = 3
 pin_hit_rad = pin_node_rad + 5
 default_hit_rad = node_rad + 2
+config_path = "config.json"
+
 
 class App(tk.Tk):
     def __init__(self):
@@ -35,14 +38,94 @@ class App(tk.Tk):
         tk.Button(bot_frame, text='Connect Nodes', command=self.set_connect_mode).pack(side='left')
         tk.Button(bot_frame, text='Remove Node', command=self.set_remove_mode).pack(side='left')
         tk.Button(bot_frame, text='Djikstra', command=self.set_djikstra_mode).pack(side='left')
+        tk.Button(bot_frame, text='Export', command=self.export_json).pack(side='right')
+        tk.Button(bot_frame, text='Import', command=self.import_json).pack(side='right')
 
         self.info = tk.Label(bot_frame, text='Mode: default | Nodes: 0 | Koneksi: 0', bg="#222", fg="white")
         self.info.pack(side='left', padx=10)
 
         self.canvas.bind('<Button-1>', self.on_click)
 
-        self.load_bg('img/bg_map.png')
+        self.load_bg('img/bg_map2.png')
+
         self.update_info()
+
+        self.nodes = []
+        self.connections = []
+
+        self.spawn_nodes_from_data()
+
+    def export_json(self):
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=".json",
+            filetypes=[("JSON Files", "*.json")]
+        )
+        if not file_path:
+            return
+
+        data = {
+            "nodes": [],
+            "connections": self.connections
+        }
+
+        for node in self.nodes:
+            if node["type"] == "pin":
+                data["nodes"].append({
+                    "type": "pin",
+                    "x": node["x"],
+                    "y": node["y"],
+                    "name": node["name"]
+                })
+            elif node["type"] == "default":
+                data["nodes"].append({
+                    "type": "default",
+                    "x": node["x"],
+                    "y": node["y"]
+                })
+
+        with open(file_path, "w") as f:
+            json.dump(data, f, indent=4)
+
+        print("Export berhasil:", file_path)
+
+    def import_json(self):
+        file_path = filedialog.askopenfilename(
+            filetypes=[("JSON Files", "*.json")]
+        )
+        if not file_path:
+            return
+
+        with open(file_path, "r") as f:
+            data = json.load(f)
+
+        self.nodes = []
+        self.connections = []
+        self.canvas.delete("all")
+
+        if self.bg_image:
+            self.canvas.create_image(0, 0, image=self.bg_image, anchor='nw')
+
+        for n in data["nodes"]:
+            if n["type"] == "pin":
+                x, y, name = n["x"], n["y"], n["name"]
+                px, py, pname, node_id, text_id = self.draw_pin_node(x, y, name)
+                self.nodes.append({"x": x, "y": y, "name": name, "type": "pin",
+                                "node_id": node_id, "text_id": text_id})
+            else:
+                x, y = n["x"], n["y"]
+                x2, y2, node_id = self.draw_default_node(x, y)
+                self.nodes.append({"x": x, "y": y, "type": "default",
+                                "node_id": node_id})
+
+        for a, b in data["connections"]:
+            self.connections.append((a, b))
+
+            x1, y1 = self.nodes[a]['x'], self.nodes[a]['y']
+            x2, y2 = self.nodes[b]['x'], self.nodes[b]['y']
+
+            self.canvas.create_line(x1, y1, x2, y2, fill='black', width=2)
+
+        print("Import berhasil", file_path)
 
     def load_bg(self, file_path):
         if not os.path.exists(file_path):
@@ -53,6 +136,50 @@ class App(tk.Tk):
         image = image.resize((canvas_w, canvas_h), Image.Resampling.LANCZOS)
         self.bg_image = ImageTk.PhotoImage(image)
         self.canvas.create_image(0, 0, image=self.bg_image, anchor='nw')
+    
+    def spawn_nodes_from_data(self):
+        new_nodes = []
+    
+        for node in self.nodes:
+            if len(node) == 5:
+                x, y, name = node[0], node[1], node[2]
+                new_node = self.draw_pin_node(x, y, name)
+                new_nodes.append(new_node)
+
+            elif len(node) == 3:
+                # default node
+                x, y = node[0], node[1]
+                new_node = self.draw_default_node(x, y)
+                new_nodes.append(new_node)
+
+        self.nodes = new_nodes
+
+        for (a, b) in self.connections:
+            x1, y1 = a
+            x2, y2 = b
+            self.draw_connection(x1, y1, x2, y2)
+
+    def draw_pin_node(self, x, y, name):
+        node_id = self.canvas.create_oval(
+            x - pin_node_rad, y - pin_node_rad,
+            x + pin_node_rad, y + pin_node_rad,
+            fill='green', outline='black', width=3
+        )
+        text_id = self.canvas.create_text(
+            x, y - 30, text=name,
+            fill='black', font=('Arial', 12, 'bold')
+        )
+        return (x, y, name, node_id, text_id)
+    
+    def draw_default_node(self, x, y):
+        node_id = self.canvas.create_oval(
+            x - node_rad, y - node_rad,
+            x + node_rad, y + node_rad,
+            fill='lightblue', outline='black', width=1.5
+        )
+        return (x, y, node_id)
+    def draw_connection(self, x1, y1, x2, y2):
+        self.canvas.create_line(x1, y1, x2, y2, fill='black', width=2)
 
     def set_pin_mode(self):
         self.mode = 'pin'
@@ -239,7 +366,7 @@ class App(tk.Tk):
             self.path_lines.append(line_id)
 
         names = [self.nodes[i].get('name', f'node{i}') for i in path]
-        messagebox.showinfo('Djikstra', f'Jalur terpendek: {" -> ".join(names)}\nJarak: {dist[end_index]:.2f}')
+        messagebox.showinfo('Djikstra', f'Node yang dilalui: {len(path)}\nJarak: {dist[end_index]:.2f}')
         self.selected_node = None
         self.update_info()
 
